e8d849c575e9ea7d0ffd7cebeb5ff56e8f899695
MegaBytten <OmegaBytten@gmail.com>
2024-10-13 19:30:13 +0100

    1.3.4 - Forge installation destination

--- firsttime_forge.sh
+++ firsttime_forge.sh
@@ -14,14 +14,19 @@ CONFIG_FILE="${PROJECT_DIR}/config.yaml"
 echo "Reading ubuntu_path from config.yaml..."
 UBUNTU_PATH=$(yq -r '.ubuntu_path' "$CONFIG_FILE")
 
+# Downloading forge installer
 echo "creating $UBUNTU_PATH/forge path and downloading forge..."
 mkdir $UBUNTU_PATH/forge
 wget -O $UBUNTU_PATH/forge/forge-1.21-installer.jar https://maven.minecraftforge.net/net/minecraftforge/forge/1.21.1-52.0.20/forge-1.21.1-52.0.20-installer.jar >/dev/null
 
-echo "installing forge..."
-cd $UBUNTU_PATH/forge
-java -jar $UBUNTU_PATH/forge/forge-1.21-installer.jar --installServer >/dev/null
-cd $UBUNTU_PATH
+# Copying forge installer to mcserver folder, installing forge, then removing copied installer
+echo "Copying installer and beginning forge installation inside $UBUNTU_PATH/mcserver..."
+cp $UBUNTU_PATH/forge/forge-1.21-installer.jar $UBUNTU_PATH/mcserver
+cd $UBUNTU_PATH/mcserver # moving in here so installation occurs inside mcserver dir
+java -jar forge-1.21-installer.jar --installServer >/dev/null
+sudo rm forge-1.21-installer.jar # removing installer
+mv forge-1.21.1-52.0.20-shim.jar forge-1.21.1-server.jar # renaming forge.jar
+cd $UBUNTU_PATH # return to normal WD
 
-echo "Moving forge server.jar to $UBUNTU_PATH/mcserver/"
-cp $UBUNTU_PATH/forge/forge-1.21.1-52.0.20-shim.jar $UBUNTU_PATH/mcserver
+echo "Forge Installation complete! Now run server for first time using:"
+echo "java -Xms10G -Xmx10G -jar forge-1.21.1-server.jar nogui"
