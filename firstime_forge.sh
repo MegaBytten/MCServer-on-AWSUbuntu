ac418e801411e73bab9dee48e2d7f55938a61ee1
MegaBytten <OmegaBytten@gmail.com>
2024-10-13 19:11:23 +0100

    1.3 - Forge initial

--- firstime_forge.sh
+++ firstime_forge.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# USAGE NOTE:
-# requires already running firsttime_install.sh
-# This gets and stores latest world download to /home/ubuntu/mcserver
-
 # Ensure the script is run with sudo privileges
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root or with sudo."
@@ -23,12 +19,12 @@ CONFIG_FILE="${PROJECT_DIR}/config.yaml"
 echo "Reading ubuntu_path from config.yaml..."
 UBUNTU_PATH=$(yq -r '.ubuntu_path' "$CONFIG_FILE")
 
-# Install buildtools and run it
-echo "installing buildtools in $UBUNTU_PATH/buildtools"
-mkdir $UBUNTU_PATH/buildtools && cd $UBUNTU_PATH/buildtools 
-wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar >/dev/null
-java -jar BuildTools.jar >/dev/null
-cd $UBUNTU_PATH
+echo "creating $UBUNTU_PATH/forge path and downloading forge..."
+mkdir $UBUNTU_PATH/forge
+wget -O $UBUNTU_PATH/forge/forge-1.21-installer.jar https://maven.minecraftforge.net/net/minecraftforge/forge/1.21.1-52.0.20/forge-1.21.1-52.0.20-installer.jar >/dev/null
 
-# move spigot files over to world file
-echo "doing stuff..."
+echo "installing forge..."
+java -jar $UBUNTU_PATH/forge/forge-1.21-installer.jar --installServer >/dev/null
+
+echo "Moving forge server.jar to $UBUNTU_PATH/mcserver/
+cp $UBUNTU_PATH/forge/forge-1.21.1-52.0.20-shim.jar $UBUNTU_PATH/mcserver
